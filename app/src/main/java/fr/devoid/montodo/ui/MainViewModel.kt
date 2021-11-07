862f49effcf3a337c84885a1d3b05cb163442ef2
Devoid <devoid.dev@gmail.com>
2021-11-07 16:31:41 +0100

    Add MainViewModel

---
+++ app/src/main/java/fr/devoid/montodo/ui/MainViewModel.kt
@@ -0,0 +1,35 @@
+package fr.devoid.montodo.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import fr.devoid.montodo.data.TodoItem
+import fr.devoid.montodo.data.TodoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val todoRepository: TodoRepository
+) : ViewModel() {
+
+    val todoItemsFlow = todoRepository.getTodoItemsFlow()
+
+    fun onTodoItemCheck(todoItem: TodoItem) = viewModelScope.launch(Dispatchers.IO) {
+        val isTodoDone = todoItem.doneAt != null
+
+        if (isTodoDone) {
+            val newTodoItem = todoItem.copy(doneAt = null)
+            todoRepository.updateTodo(newTodoItem)
+        } else {
+            val newTodoItem = todoItem.copy(doneAt = System.currentTimeMillis())
+            todoRepository.updateTodo(newTodoItem)
+        }
+    }
+
+    fun onTodoDelete(todoItem: TodoItem) = viewModelScope.launch(Dispatchers.IO) {
+        todoRepository.deleteTodo(todoItem)
+    }
+
+}
